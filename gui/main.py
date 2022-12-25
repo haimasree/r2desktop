2060d738cd2023b62a91233bb6649ed2c4379067
Haimasree <haimasree.il@gmail.com>
2022-12-25 12:33:28 +0200

    moving to main function

--- gui/main.py
+++ gui/main.py
@@ -80,22 +80,23 @@ def save_file():
         output_file.write(text)
     window.title(f"Simple Text Editor - {filepath}")
 
-window = tk.Tk()
-window.title("Simple Text Editor")
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.title("Simple Text Editor")
 
-window.rowconfigure(0, minsize=800, weight=1)
-window.columnconfigure(1, minsize=800, weight=1)
+    window.rowconfigure(0, minsize=800, weight=1)
+    window.columnconfigure(1, minsize=800, weight=1)
 
-txt_edit = tk.Text(window)
-frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
-btn_save = tk.Button(frm_buttons, text="Save As...", command=save_file)
+    txt_edit = tk.Text(window)
+    frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+    btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
+    btn_save = tk.Button(frm_buttons, text="Save As...", command=save_file)
 
 
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+    btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 
-frm_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
+    frm_buttons.grid(row=0, column=0, sticky="ns")
+    txt_edit.grid(row=0, column=1, sticky="nsew")
 
-window.mainloop()
+    window.mainloop()

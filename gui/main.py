d8d8fdf151e87e2cdd65000550060e25b8c45a86
Haimasree <haimasree.il@gmail.com>
2023-01-16 12:37:37 +0200

    cleanup

--- gui/main.py
+++ gui/main.py
@@ -1,43 +1,26 @@
 import tkinter as tk
 import tkinter.filedialog as fd
-
-import rpy2.robjects as robjects
-
 import subprocess
 import sys
 
+
 def run_telomere():
-    
+
     # with open("test.log", "wb") as f:
-    process = subprocess.Popen(["Rscript", "--vanilla", "Telomere-Analyzer/nanotel-multicore-10workers.R", input_dir.get(), output_dir.get(), file_type.get()], stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        [
+            "Rscript",
+            "--vanilla",
+            "Telomere-Analyzer/nanotel-multicore-10workers.R",
+            input_dir.get(),
+            output_dir.get(),
+            file_type.get(),
+        ],
+        stdout=subprocess.PIPE,
+    )
     for c in iter(lambda: process.stdout.read(1), b""):
         sys.stdout.buffer.write(c)
-        # f.buffer.write(c)
-    # output = subprocess.run(
-    #     [
-    #         "Rscript",  # Assumption: will not run in windows anymore
-    #         "--vanilla",
-    #         "Telomere-Analyzer/nanotel-multicore-10workers.R",
-    #         input_dir.get(), 
-    #         output_dir.get(), 
-    #         file_type.get()
-    #     ],
-    #     stdout=subprocess.PIPE, 
-    #     stderr=subprocess.PIPE, 
-    #     text=True,
-    # )
-    # if output.returncode:
-    #     raise IOError(output)
-    # subprocess.run()  
-    # r = robjects.r
-    # r["source"]("Telomere-Analyzer/nanotel-multicore-10workers.R")(input_dir.get(), output_dir.get(), (file_type.get()])
-    # # telomere_r = robjects.globalenv["capitalize_input"]
-    # # output_str_r = telomere_r(
-    # #     input_dir.get(), output_dir.get(), file_type.get()
-    # # )[]
-    # print(f"Output from R = {telomere_r}")
-
-
+    
 def get_input():
     input_dir.set(fd.askdirectory())
     window.title(f"Telomere-Analyzer - {input_dir.get()}")
